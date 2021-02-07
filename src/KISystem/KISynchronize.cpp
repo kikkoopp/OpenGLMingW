3ba768c7ffa024ad96db2a02f7640fcbee442028
cyf <yifei.chen@xiaoxitech.com>
2021-02-07 14:53:42 +0800

    Synchronize 模块

--- src/KISystem/KISynchronize.cpp
+++ src/KISystem/KISynchronize.cpp
@@ -3,3 +3,127 @@
 //
 
 #include "KISynchronize.h"
+using namespace KIEngine;
+KICriticalSection g_Safes;
+
+void KISynchronize::KISafeOutPutDebugString(const TCHAR *pString, ...) {
+    g_Safes.Lock();
+    char* pArgs;
+    pArgs = (char*)&pString + sizeof(pString);
+    _vstprintf_s( KISystem::ms_sLogBuffer, LOG_BUFFER_SIZE, pString, pArgs );
+    OutputDebugString( KISystem::ms_sLogBuffer );
+    g_Safes.UnLock();
+}
+
+unsigned int
+KISynchronize::WaitAll(KISynchronize **pSynchronize, unsigned int uiNum, bool bWaitAll, DWORD dwMilliseseconds) {
+    KIMAC_ASSERT( uiNum >= 1 && uiNum <= MAXIMUM_WAIT_OBJECTS );
+    static HANDLE handle[MAXIMUM_WAIT_OBJECTS];
+    for ( unsigned int i = 0; i < uiNum; ++i ) {
+        handle[i] = (HANDLE)pSynchronize[i]->GetHandle();
+    }
+    DWORD dw = WaitForMultipleObjects(uiNum, handle, bWaitAll, dwMilliseseconds);
+    switch(dw)
+    {
+        case WAIT_FAILED:
+            return WF_FAILED;
+        case WAIT_TIMEOUT:
+            return WF_TIMEOUT;
+        case WAIT_OBJECT_0:
+            return WF_OBJECT0;
+
+    }
+    return WF_FAILED;
+}
+
+KIEvent::KIEvent() {
+    Event = NULL;
+}
+
+KIEvent::~KIEvent() {
+    if ( Event != NULL ) {
+        CloseHandle( Event );
+    }
+}
+
+void KIEvent::Lock() {
+    WaitForSingleObject( Event, INFINITE );
+}
+
+void KIEvent::UnLock() {
+    PulseEvent( Event );
+}
+
+bool KIEvent::Create(bool bIsManualReset, const TCHAR *InName) {
+    Event = CreateEvent( NULL, bIsManualReset, 0, InName );
+    return Event != NULL;
+}
+
+void KIEvent::Trigger() {
+    SetEvent( Event );
+}
+
+void KIEvent::Reset() {
+    ResetEvent( Event );
+}
+
+void KIEvent::Pulse() {
+    PulseEvent( Event );
+}
+
+bool KIEvent::Wait(DWORD WaitTime) {
+    return WaitForSingleObject( Event, WaitTime ) == WAIT_OBJECT_0;
+}
+
+bool KIEvent::IsTrigger() {
+    return Wait(0);
+}
+
+KIMutex::KIMutex() {
+    m_mutex = CreateMutex( NULL, FALSE, NULL );
+    KIMAC_ASSERT( m_mutex );
+}
+
+KIMutex::~KIMutex() noexcept {
+    BOOL closed = CloseHandle( (HANDLE)m_mutex );
+    m_mutex = NULL;
+    KIMAC_ASSERT( closed );
+}
+
+void KIMutex::Enter() {
+    DWORD result = WaitForSingleObject( (HANDLE)m_mutex, INFINITE );
+    KIMAC_ASSERT( result != WAIT_FAILED );
+}
+
+void KIMutex::Leave() {
+    BOOL released = ReleaseMutex( (HANDLE)m_mutex );
+    KIMAC_ASSERT( released );
+}
+
+KISemaphore::KISemaphore(unsigned int uiCount, unsigned int uiMaxCount) {
+    KIMAC_ASSERT( uiCount <= uiMaxCount );
+    Semaphore = CreateSemaphore( NULL, uiCount, uiMaxCount, NULL );
+    m_uiMaxCount = m_uiMaxCount;
+    KIMAC_ASSERT( Semaphore );
+}
+
+KISemaphore::~KISemaphore() noexcept {
+    BOOL closed = CloseHandle( (HANDLE)Semaphore );
+    Semaphore = NULL;
+    KIMAC_ASSERT( closed );
+}
+
+void KISemaphore::Enter() {
+    DWORD result = WaitForSingleObject( (HANDLE)Semaphore, INFINITE );
+    KIMAC_ASSERT( result );
+}
+
+void KISemaphore::Leave(unsigned int uiReleaseCount) {
+    BOOL released = ReleaseSemaphore( (HANDLE)Semaphore, uiReleaseCount, NULL );
+    KIMAC_ASSERT( released );
+}
+
+
+
+
+
--- src/KISystem/KISynchronize.h
+++ src/KISystem/KISynchronize.h
@@ -5,8 +5,11 @@
 #ifndef OPENGLEXAMPLE_KISYNCHRONIZE_H
 #define OPENGLEXAMPLE_KISYNCHRONIZE_H
 
+#include <cmath>
+#include <synchapi.h>
+#include <handleapi.h>
+#include <eventsys.h>
 #include "KISystem.h"
-#include "synchapi.h"
 
 namespace KIEngine {
     class KISYSTEM_API KISynchronize {
@@ -67,6 +70,101 @@ namespace KIEngine {
             LeaveCriticalSection(&CriticalSection);
         }
     };
+
+    /**
+     * 信号量,是一种加减操作
+     */
+    class KISYSTEM_API KISemaphore{
+    public:
+        KISemaphore( unsigned int uiCount, unsigned int uiMaxCount );
+        virtual ~KISemaphore();
+
+        virtual void Enter();
+        virtual void Leave( unsigned int uiReleaseCount );
+        virtual void* GetHandle() { return Semaphore; }
+    protected:
+        void* Semaphore;
+        unsigned int m_uiMaxCount;
+    };
+
+    /**
+     * 互斥锁
+     */
+    class KISYSTEM_API KIMutex : public KISynchronize {
+    public:
+        KIMutex();
+        virtual ~KIMutex();
+
+        virtual void Enter();
+        virtual void Leave();
+        virtual void* GetHandle() { return m_mutex; }
+    protected:
+        void* m_mutex;
+    };
+
+    /**
+     * windows 事件
+     */
+    class KISYSTEM_API KIEvent : public KISynchronize {
+        HANDLE Event;
+    public:
+        virtual void* GetHandle() { return Event; }
+        /**
+         * 构造，将event handle置为空
+         */
+        KIEvent();
+        /**
+         * 销毁event handle
+         */
+        virtual ~KIEvent();
+        /**
+         * 尝试获取event，获取不到就阻塞
+         */
+        virtual void Lock();
+        /**
+         * 发送一个脉冲，然后重置事件设置为无信号
+         * 只有足够快的线程才能够接受到这个脉冲
+         */
+        virtual void UnLock();
+
+        /**
+         * 创建一个事件
+         * @param bIsManualReset 是否为手动重置事件
+         * @param InName 事件名称
+         * @return 是否成功创建
+         */
+        virtual bool Create( bool bIsManualReset = FALSE, const TCHAR* InName = NULL );
+
+        /**
+         * 触发事件
+         */
+        virtual void Trigger( void );
+
+        /**
+         * 重置事件
+         * 如果事件是手动重置，那么需要调用重置
+         * 来将事件设置为无信号状态
+         */
+        virtual void Reset( void );
+
+        /**
+         * 触发事件，发送脉冲，然后重置事件
+         * 对于手动重置事件，所有的线程都会被释放
+         * 对于自动重置事件，只有一个线程会被释放
+         */
+        virtual void Pulse( void );
+
+        /**
+         * 等待事件被触发
+         * @param WaitTime
+         * @return
+         */
+        virtual bool Wait( DWORD WaitTime = (DWORD)-1 );
+
+        virtual bool IsTrigger();
+
+    };
+
 
 }
 
